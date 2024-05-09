8285902d65b9430976bd2f134a05e23e6d70b542
unknown <retoy1970@gmail.com>
2024-05-09 10:15:24 +0330

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const img1=document.getElementById("img1")
+
+
+function toggle(){
+    if(img1.style.display=='none'){
+        img1.style.display='block'
+
+    }else{
+        img1.style.display='none'
+    }
+}
